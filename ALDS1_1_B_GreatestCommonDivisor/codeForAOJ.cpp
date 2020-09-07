4305e56b5676cadbcaca9b35b3316e097ddeb3b9
HYHY3 <hyhy.cyt@gmail.com>
2020-09-07 11:20:01 +0900

    completed ver2.0

--- ALDS1_1_B_GreatestCommonDivisor/codeForAOJ.cpp
+++ ALDS1_1_B_GreatestCommonDivisor/codeForAOJ.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+int getGCD(int a, int b) {
+	int ans;
+
+	if (b == 0) {
+		return a;
+	}
+	ans = getGCD(b, a%b);
+	return ans;
+	//return getGCD(b, a%b);
+}
+
 int main() {
-	int x, y, a;
+	int x, y;
 
 	cin >> x >> y;
-
-	do {
-		a = x % y;
-		x = y;
-		y = a;
-	} while (a != 0);
-
-	cout << x << endl;
+	cout << getGCD(x, y) << endl;
 
 	return 0;
 }
