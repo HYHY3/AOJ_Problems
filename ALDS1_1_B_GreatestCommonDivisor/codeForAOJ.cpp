b92e02538c3660f673d91be74944917a7a0147b9
HYHY3 <hyhy.cyt@gmail.com>
2020-09-07 11:11:45 +0900

    completed ver1.0

---
+++ ALDS1_1_B_GreatestCommonDivisor/codeForAOJ.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int x, y, a;
+
+	cin >> x >> y;
+
+	do {
+		a = x % y;
+		x = y;
+		y = a;
+	} while (a != 0);
+
+	cout << x << endl;
+
+	return 0;
+}
